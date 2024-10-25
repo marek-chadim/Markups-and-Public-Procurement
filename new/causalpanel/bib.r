68cfddb004c070b839aa9638080f10448569035a
Marek Chadim <chadimarek@gmail.com>
2024-10-25 08:25:58 +0200

    lalonde

--- new/causalpanel/bib.r
+++
@@ -1,3 +0,0 @@
-install.packages("rebib")
-path_to_your_file <- "main_2024_06_20.bbl"
-rebib::biblio_converter(path_to_your_file)
--- new/did_imputation/bib.r
+++
@@ -1,3 +0,0 @@
-install.packages("rebib")
-path_to_your_file <- "ms.bbl"
-rebib::biblio_converter(path_to_your_file)
